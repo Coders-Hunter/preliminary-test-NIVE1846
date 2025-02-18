b2f7b939c48fc45259916b919625a2d5e2b5fc00
NIVE1846 <dnivetha810@gmail.com>
2025-02-18 05:48:08 -0500

    Your commit message

--- python/sum_function.py
+++ python/sum_function.py
@@ -1,5 +1,5 @@
 def sum_numbers(num1, num2):
-num1 = int(input("Enter first number: "))
+  num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
-num3 = num1+num2
-print("Sum:", sum_numbers(num3))
+
+print("Sum:", sum_numbers(num1,num2))
